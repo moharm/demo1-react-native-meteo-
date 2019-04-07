97f2f0650e894987fc1d38361c5a1f9e5e2d014a
unknown <moharm123@gmail.com>
2019-04-08 01:02:05 +0200

    second commit

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import About from './components/About'
 import Search from './components/search'
+import {TabNavigator} from 'react-navigation'
+
+  const Tabs = TabNavigator ({
+    Search : {screen: Search},
+    About : {screen: About}
+  })
 
 export default class App extends React.Component {
+
   render() {
     return (
-      <View style={{marginVertical:40}}>
-        <Search/>
-      </View>
+      <Tabs/>
     );
   }
 }
